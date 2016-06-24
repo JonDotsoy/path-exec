e62211d1802022bd37431d0e4b212b379b448b19
Jonathan Delgado hi@jon.soy <hi@jon.soy>
2016-06-24 19:00:39 -0400

    * add test * use multiple executions

--- test/PathsExecText.js
+++ test/PathsExecText.js
@@ -1,19 +1,55 @@
+const expect = require( 'expect.js' )
 const PathsExec = require( '..' )
 
 let paths = new PathsExec()
 
+describe( 'PathsExec', function() {
+	let countME = 0
 
-paths.use( '/:name', [], function( params, next ) {
-	console.log( params instanceof PathsExec.Params )
-	next()
-}, function( params, next ) {
-	console.log( 'hola' )
-	next()
-} )
+	describe( 'define uses', function() {
 
+		it( 'one Execution with /one/:number', () => {
+			paths.use( '/one/:number', [ 'number' ], function( params ) {
+				expect( params.number ).not.to.be( undefined )
+				expect( params.number ).to.be( '32' )
+			} )
+		} )
 
-paths
-	.exec( '/quehay' )
-	.then( () => {
-		console.log( 'next load' )
+		it( 'multiple Executions with /two/:number', () => {
+			paths.use( '/two/:number', [ 'number' ], function( params ) {
+				expect( params.number ).not.to.be( undefined )
+				expect( params.number ).to.be( '12' )
+				countME = countME + 1
+			}, function( params ) {
+				countME = countME + 1
+			} )
+		} )
+
 	} )
+
+	describe( 'Execute', function() {
+
+		it( '/one/32', function( next ) {
+			paths.exec( '/one/32' )
+				.then( () => {
+					next()
+				} )
+				.catch( ( err ) => {
+					next( err )
+				} )
+		} )
+
+		it( '/two/12', ( next ) => {
+			paths.exec( '/two/12' )
+				.then( () => {
+					expect( countME ).to.be( 2 )
+					next()
+				} )
+				.catch( err => {
+					next( err )
+				} )
+		} )
+
+	} )
+
+} )
